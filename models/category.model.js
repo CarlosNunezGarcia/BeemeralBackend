dbd9bea06bcb2f78bd4db0fef66f0617cac11350
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-24 11:51:03 +0200

    feat: category model added

---
+++ models/category.model.js
@@ -0,0 +1,39 @@
+// Importa los módulos necesarios
+const { DataTypes } = require('sequelize');
+const db = require('../config.json'); // Archivo de configuración de la base de datos
+const Supercategory = require('./supercategory.model'); // Modelo de la tabla "supercategory"
+
+// Define el modelo "Category"
+const Category = db.define('category', {
+  category_id: {
+    type: DataTypes.SMALLINT.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  category_title: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  is_deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  supercategory_id: {
+    type: DataTypes.SMALLINT.UNSIGNED,
+    allowNull: false,
+    references: {
+      model: Supercategory,
+      key: 'supercategory_id'
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+  }
+}, {
+  // Nombre de la tabla en la base de datos
+  tableName: 'category',
+  // Opciones adicionales del modelo
+  timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
+});
+
+// Exporta el modelo
+module.exports = Category;
