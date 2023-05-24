bfa77d3704668a252ad1164a82fe6f4cfa98ae23
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-24 11:57:32 +0200

    feat: category fixed

--- models/category.model.js
+++ models/category.model.js
@@ -5,35 +5,40 @@ const Supercategory = require('./supercategory.model'); // Modelo de la tabla "s
 
 // Define el modelo "Category"
 const Category = db.define('category', {
-  category_id: {
-    type: DataTypes.SMALLINT.UNSIGNED,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  category_title: {
-    type: DataTypes.STRING(50),
-    allowNull: false
-  },
-  is_deleted: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  },
-  supercategory_id: {
-    type: DataTypes.SMALLINT.UNSIGNED,
-    allowNull: false,
-    references: {
-      model: Supercategory,
-      key: 'supercategory_id'
+    category_id: {
+      type: DataTypes.SMALLINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
     },
+    category_title: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    supercategory_id: {
+      type: DataTypes.SMALLINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: Supercategory,
+        key: 'supercategory_id'
+      }
+    }
+  }, {
+    // Nombre de la tabla en la base de datos
+    tableName: 'category',
+    // Opciones adicionales del modelo
+    timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
+  });
+  
+  // Crea la restricción de clave externa
+  Category.belongsTo(Supercategory, {
+    foreignKey: 'supercategory_id',
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE'
-  }
-}, {
-  // Nombre de la tabla en la base de datos
-  tableName: 'category',
-  // Opciones adicionales del modelo
-  timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
-});
-
-// Exporta el modelo
-module.exports = Category;
+  });
+  
+  // Exporta el modelo
+  module.exports = Category;
