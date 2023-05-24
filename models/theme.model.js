2a898e730ece60bf71ae73e12eae28406ddd0337
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-24 13:45:44 +0200

    feat: theme added

---
+++ models/theme.model.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config.json'); // Archivo de configuración de la base de datos
+
+const Theme = db.define('theme', {
+    theme_id: {
+      type: DataTypes.SMALLINT,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    theme_title: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    tableName: 'theme',
+    timestamps: false
+  });
+  
+  module.exports = Theme;
