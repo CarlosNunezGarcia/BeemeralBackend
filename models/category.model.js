7e623b287e309f4b5e1bdf971412d235034ef23d
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-25 09:20:42 +0200

    feat: models fixed

--- models/category.model.js
+++ models/category.model.js
@@ -1,44 +1,44 @@
 // Importa los módulos necesarios
 const { DataTypes } = require('sequelize');
-const db = require('../config.json'); // Archivo de configuración de la base de datos
 const Supercategory = require('./supercategory.model'); // Modelo de la tabla "supercategory"
 
 // Define el modelo "Category"
-const Category = db.define('category', {
-    category_id: {
-      type: DataTypes.SMALLINT.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    category_title: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    is_deleted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    supercategory_id: {
-      type: DataTypes.SMALLINT.UNSIGNED,
-      allowNull: false,
-      references: {
-        model: Supercategory,
-        key: 'supercategory_id'
+function modelCategory(sequelize){
+
+  const Category = sequelize.define('category', {
+      category_id: {
+        type: DataTypes.SMALLINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      category_title: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      is_deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      supercategory_id: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
       }
+    }, {
+      // Nombre de la tabla en la base de datos
+      tableName: 'category',
+      // Opciones adicionales del modelo
+      timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
+    });
+    
+    // Crea la restricción de clave externa
+    Category.associate = (models) => {
+      Category.belongsTo(Supercategory, {
+        foreignKey: 'supercategory_id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
-  }, {
-    // Nombre de la tabla en la base de datos
-    tableName: 'category',
-    // Opciones adicionales del modelo
-    timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
-  });
-  
-  // Crea la restricción de clave externa
-  Category.belongsTo(Supercategory, {
-    foreignKey: 'supercategory_id',
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
-  });
+}
   
   // Exporta el modelo
-  module.exports = Category;
+  module.exports = modelCategory;
--- models/supercategory.model.js
+++ models/supercategory.model.js
@@ -1,26 +1,29 @@
 const { DataTypes } = require('sequelize');
 
 
-const Supercategory = db.define('supercategory', {
-    supercategory_id: {
-      type: DataTypes.SMALLINT.UNSIGNED,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    supercategory_title: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    is_deleted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
-  }, {
-    // Nombre de la tabla en la base de datos
-    tableName: 'supercategory',
-    // Opciones adicionales del modelo
-    timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
-  });
+function modelSupercategory(sequelize){
+
+  const Supercategory = sequelize.define('supercategory', {
+      supercategory_id: {
+        type: DataTypes.SMALLINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      supercategory_title: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      is_deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    }, {
+      // Nombre de la tabla en la base de datos
+      tableName: 'supercategory',
+      // Opciones adicionales del modelo
+      timestamps: false // Si no hay columnas de createdAt y updatedAt en la tabla
+    });
+}
   
   // Exporta el modelo
-  module.exports = Supercategory;
+  module.exports = modelSupercategory;
