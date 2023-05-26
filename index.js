9c9e3c146b6dd3a9537a35fbd21a4255170dc189
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-26 09:06:47 +0200

    feat: index modified

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("rootpath")();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const errorHandler = require("_middlewares/error-handler");
 const { sequelize } = require("sequelize");
@@ -12,10 +15,17 @@ const subscriptionsRoutes = require("./routes/subscription.routes");
 const adminRoutes = require("./routes/admin.routes");
 const projectRoutes = require("./routes/project.routes");
 
+//view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+//middleware
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "*"}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // api routes
 app.use("/users", usersRoutes);
@@ -34,3 +44,5 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log("Database & tables created!");
   app.listen(4000, () => console.log(`Example app listening on port ${port}!`));
 });
+
+module.exports = app;
