832525f0f9abd69ad2288a333f4682e6d09fcdb9
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-26 11:16:11 +0200

    feat: billing routes added

---
+++ routes/billing.routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const billingController = require('../controllers/billing.controller');
+const auth = require('../_middlewares/authorize');
+
+//get all prices
+router.get('/prices', auth(), billingController.getPrices);
+
+//get stripe´s price trough priceId
+router.get('/onePrice', billingController.getOnePrice);
+
+//check subscription´s status
+router.get('/getSubscriptionStatus', billingController.subscriptionStatus);
+
+//Check all the data of a client, the status of your subscription, I check that it exists in Stripe,
+//checks the status of your subscription with Stripe if you have it and compares it with the information of that user and subscription that we have in our database
+//If necessary, update the DB with the information obtained, if the subscription has been renewed (through the last date saved in the DB)
+router.put("/getOneCustomer", auth, billingController.getOneCustomer);
+
+//check Last Transaction
+//put || get
+router.get("/getLastTransaction", billingController.checkLastTransactions);
+
+//get All customers
+router.get("/getAllCustomers", billingController.getAllActiveCustomers);
+
+//delete subscription
+router.put("/deleteUserSubscription", auth, billingController.deleteSubscription);
+
+//update db (export && render) after payment
+router.put("/updateBuyItem", auth, billingController.updateAfterBuyOneItem);
+
+//check subscription´s status
+router.put("/checkPurchases", verify ,billingController.getAndCheckCurrentSubscription);
+
+//get user purchases
+router.get("/userCharges", auth, billingController.getUserPurchases);
+
+//create payment intent
+router.put("/createPaymentIntent", auth, billingController.createPaymentIntent);
+
+module.exports = router;
