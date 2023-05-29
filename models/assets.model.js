d50ec525a1677c1b847a8a994b709820cae36886
Carlos Núñez García <carlosnunez@beemeral.com>
2023-05-29 11:36:24 +0200

    feat: assets model added

---
+++ models/assets.model.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+
+// Define el modelo "Asset"
+function modelAsset(sequelize){
+
+    const Asset = sequelize.define('asset', {
+
+        asset_id: {
+            type: DataTypes.SMALLINT,   
+            primaryKey: true,
+            autoIncrement: true
+        },
+        asset_name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        asset_type: {
+            type: DataTypes.INTEGER(7),
+            allowNull: false
+        },
+        asset_tags: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        asset_reference: {
+            type: DataTypes.VARCHAR(255),
+            allowNull: false
+        },
+    });
+};
+
+// Exporta el modelo
+module.exports = modelAsset;
